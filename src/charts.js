be699a4a09fc3ff74a84769c8f17f370db757ac4
Kari2406 <karina.sremigio@gmail.com>
2020-03-17 19:02:06 -0600

    data lovers Patients Kari y Yeraldin

--- src/charts.js
+++ src/charts.js
@@ -83,21 +83,26 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-var ctx = document.getElementById('myChart2-');
+var ctx = document.getElementById('myChart2-').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'scatter',
+    type: 'bar',
     data: {
-        labels: ['Español', 'Inglés', 'Chino'],
+        labels: ['No hispano', 'Puertorriqueño', 'Centroamericano'],
         datasets: [{
-            label: '# of Votes',
-            data: [44, 4, 2],
+            label: 'No pude quitar esto :v',
+            data: [46, 3, 1],
             backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
@@ -105,9 +110,42 @@ var myChart = new Chart(ctx, {
             borderWidth: 1
         }]
     },
-
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
 var ctx = document.getElementById('myChart3').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'polarArea',
+
+    // The data for our dataset
+    data: {
+        labels: ['Solteros', 'Casados'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            data: [15, 35]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+var ctx = document.getElementById('myChart3-').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'polarArea',
