826d9773ba6c1aa459963fc44e695378f921df87
Kari2406 <karina.sremigio@gmail.com>
2020-03-18 08:26:01 -0600

    git pages

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 // import data from './data/injuries/injuries.js';
 // import data from './data/lol/lol.js';
- import data from './data/patient/patient.js';
+import data from './data/patient/patient.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/steam/steam.js';
