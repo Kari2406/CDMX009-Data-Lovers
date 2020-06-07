7e077747e2e4a0b59480d19b0e4f4349bb42e279
Kari2406 <karina.sremigio@gmail.com>
2020-06-07 16:57:52 -0500

    adding complete project

--- docs/main.js
+++ docs/main.js
@@ -5,13 +5,13 @@ window.onload = () => {
   createTable();
 }
 const createTable = (filteredList) => {
-  /*  Table first time */
+ /*  Table first time */
   let lista = [...cleanData]
   /* Filter */
-  if (filteredList) {
+  if(filteredList){
     lista = [...filteredList]
   }
-
+  
   let showTable = document.getElementById('tab-content-table');
   showTable.innerHTML = "" // clean table
   showTable.innerHTML = `<div class="search">        
@@ -25,62 +25,59 @@ const createTable = (filteredList) => {
   tableBody.className = 'tableBody'
   table.border = '1';
   table.appendChild(tableBody);
-
-  /*   Table header */
+  
+/*   Table header */
   let heading = ['Nombre', 'Fecha de nacimiento', 'Teléfono', 'Dirección'];
   let trHeader = document.createElement('tr');
   trHeader.className = 'tableHeader'
   tableBody.appendChild(trHeader);
-  for (let i = 0; i < heading.length; i++) {
+  for (let i = 0; i < heading.length; i++){
     let th = document.createElement('th')
     th.className = 'thHeader'
     th.appendChild(document.createTextNode(heading[i]));
     trHeader.appendChild(th);
   }
-  /*  Table body */
+/*  Table body */
   for (let i = 0; i < lista.length; i++) {
-    let tr = document.createElement('tr');
-    tr.className = 'trBody'
-    let tdName = document.createElement('td');
-    let clickName = document.createElement('button');
-    clickName.appendChild(document.createTextNode(lista[i].name));
-    clickName.className = 'clickName';
-    tdName.className = 'tdBody'
-    tdName.appendChild(clickName);
-    tr.appendChild(tdName);
-    tableBody.appendChild(tr);
-
-    let tdBirthdate = document.createElement('td');
-    tdBirthdate.className = 'tdBody'
-    tdBirthdate.appendChild(document.createTextNode(lista[i].birthDate));
-    tr.appendChild(tdBirthdate);
-    tableBody.appendChild(tr);
-    let tdPhone = document.createElement('td');
-    tdPhone.className = 'tdBody'
-    tdPhone.appendChild(document.createTextNode(lista[i].phone));
-    tr.appendChild(tdPhone);
-    tableBody.appendChild(tr);
-    let tdAddress = document.createElement('td');
-    tdAddress.className = 'tdBody'
-    tdAddress.appendChild(document.createTextNode(lista[i].address));
-    tr.appendChild(tdAddress);
-    tableBody.appendChild(tr);
-    showTable.appendChild(table);
-  }
-  openModal(lista, cardPatient);
+  let tr = document.createElement('tr');
+  tr.className = 'trBody'
+  let tdName = document.createElement('td');
+  let clickName = document.createElement('button');
+  clickName.appendChild(document.createTextNode(lista[i].name));
+  clickName.className = 'clickName';
+  tdName.className = 'tdBody'
+  tdName.appendChild(clickName);
+  tr.appendChild(tdName);
+  tableBody.appendChild(tr);
+    
+  let tdBirthdate = document.createElement('td');
+  tdBirthdate.className = 'tdBody'
+  tdBirthdate.appendChild(document.createTextNode(lista[i].birthDate));
+  tr.appendChild(tdBirthdate);
+  tableBody.appendChild(tr);
+  let tdPhone = document.createElement('td');
+  tdPhone.className = 'tdBody'
+  tdPhone.appendChild(document.createTextNode(lista[i].phone));
+  tr.appendChild(tdPhone);
+  tableBody.appendChild(tr);
+  let tdAddress = document.createElement('td');
+  tdAddress.className = 'tdBody'
+  tdAddress.appendChild(document.createTextNode(lista[i].address));
+  tr.appendChild(tdAddress);
+  tableBody.appendChild(tr);
+  showTable.appendChild(table);
 }
+  openModal(lista, cardPatient);
+ }
 /* Card patients */
 let cardPatient = document.getElementsByClassName('clickName');
 const openModal = (cleanData, cardPatient) => {
-
+  
   for (let i = 0; i < cardPatient.length; i++) {
     cardPatient[i].addEventListener('click', () => {
-      document.getElementById('cardContainer').style.display = 'block';
+      document.getElementById('cardContainer').style.display='block';
       document.getElementById('cardContainer').innerHTML = `
       <div id="modal1" class="modal">
-      <div id="modalClose" class="modal-close">
-      <a href="#"><b>X</b></a>
-    </div>
           <div class="infoPatient">
             <h4 class ="titlePatient"><b>${cleanData[i].name}</b></h4>
               <ul> 
@@ -94,14 +91,16 @@ const openModal = (cleanData, cardPatient) => {
                 <li>Etnia: <b>${cleanData[i].etnia}</b></li>
               </ul>
         </div>
-
+        <div id="modalClose" class="modal-close">
+          <a href="#"><b>X</b></a>
+        </div>
       </div>
       `
       modalClose.onclick = () => {
-        document.getElementById('cardContainer').style.display = 'none';
+        document.getElementById('cardContainer').style.display='none';
       }
     });
-  }
+}
 }
 /* Filter by name */
 let arrayPatients;
@@ -111,7 +110,7 @@ let searchPatient = () => {
   createTable(arrayPatients);
 }
 /* Button */
-function setListener() {
+function setListener(){
   let searchButton = document.getElementById("search");
   searchButton.addEventListener("click", searchPatient);
 }
